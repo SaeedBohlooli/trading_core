32812b4b0b5d6afae55589ae2c379da20371473a
bs24680@gmail.com <Yek00Yek@>
2025-12-18 00:41:43 -0500

    from p106- server

--- ib_heartbeat_loop.py
+++ ib_heartbeat_loop.py
@@ -2,10 +2,11 @@ import asyncio
 from datetime import datetime, timezone
 from pathlib import Path
 from ib_async import IB
-
+from trading_core.file_manager import FileManager
+import os
 async def ib_heartbeat_loop(
     ib: IB,
-    heartbeat_file: str,
+    heartbeat_file: str = None,
     interval_seconds: int = 30,
 ):
     """
@@ -16,6 +17,8 @@ async def ib_heartbeat_loop(
     - Does NOT create a new IB client
     - Safe to run continuously
     """
+    if heartbeat_file is None:
+        heartbeat_file = os.path.join(FileManager.dirs.ib, "heartbeat.log")
 
     path = Path(heartbeat_file)
 
