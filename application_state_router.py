5a6da52eb80e6bd1cbe0f22e0cc90d74f1c41f07
Saeed Bohlooli <s.bohlooli@gmail.com>
2025-12-21 16:06:15 -0500

    from u102

---
+++ application_state_router.py
@@ -0,0 +1,10 @@
+from trading_utils import *
+
+
+
+def populate_global_state(application_state):
+    application_state['global_state.symbol_to_conid'] = global_state.symbol_to_conid
+    application_state['global_state.conid_to_symbol'] = global_state.conid_to_symbol
+    application_state['global_state.quote_cache'] = global_state.quote_cache
+    application_state['global_state.contract_cache'] = global_state.contract_cache
+    application_state['global_state.option_contract_cache'] = global_state.option_contract_cache
