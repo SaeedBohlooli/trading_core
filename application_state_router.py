0fa937c63256a8bc8683c07ebbe4b90ac713481a
Saeed Bohlooli <s.bohlooli@gmail.com>
2026-01-26 18:48:02 -0500

    from u102

--- application_state_router.py
+++ application_state_router.py
@@ -9,5 +9,6 @@ def populate_global_state(application_state):
     application_state['global_state.symbol_to_conid'] = global_state.symbol_to_conid
     application_state['global_state.conid_to_symbol'] = global_state.conid_to_symbol
     application_state['global_state.quote_cache'] = global_state.quote_cache
+    application_state['global_state.quote_cache_count'] = len(global_state.quote_cache)
     application_state['global_state.contract_cache'] = global_state.stringify_option_cache(global_state.contract_cache)
     application_state['global_state.option_contract_cache'] = global_state.stringify_option_cache(global_state.option_contract_cache)
