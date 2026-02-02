fc0d65919af6b5bbe88fa713f3b95f39b7e45846
Saeed Bohlooli <s.bohlooli@gmail.com>
2026-02-02 13:56:43 -0500

    from u102

--- application_state_router.py
+++ application_state_router.py
@@ -10,5 +10,8 @@ def populate_global_state(application_state):
     application_state['global_state.conid_to_symbol'] = global_state.conid_to_symbol
     application_state['global_state.quote_cache'] = global_state.quote_cache
     application_state['global_state.quote_cache_count'] = len(global_state.quote_cache)
+    application_state['global_state.conid_to_symbol_subscribed_for_quotes'] = global_state.conid_to_symbol_subscribed_for_quotes
     application_state['global_state.contract_cache'] = global_state.stringify_option_cache(global_state.contract_cache)
     application_state['global_state.option_contract_cache'] = global_state.stringify_option_cache(global_state.option_contract_cache)
+    application_state['global_state.quote_cache_symbols'] = global_state.extract_symbols_from_quote_cache()
+    application_state['global_state.subscribed_symbols_count'] = global_state.subscribed_symbols_count
--- streamers/state_streamer.py
+++ streamers/state_streamer.py
@@ -43,7 +43,7 @@ class StateStreamer:
 
             except Exception as e:
                 logger.error(f"[StateStreamer] @@@@ Unexpected error: {e}")
-                print(pprint.pformat(packet))
+                logger.info(pprint.pformat(packet))
                 logger.info(f"[StateStreamer] Retrying in 10 seconds...Check the message: {self.app_state} ")
                 await asyncio.sleep(self.interval)
 
